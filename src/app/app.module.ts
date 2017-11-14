8593bc54de66e341d030cfcbd760f5374a2cf0b8
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2017-11-14 11:51:03 +0100

    Create routing module Create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+/** modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
+/** components */
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
