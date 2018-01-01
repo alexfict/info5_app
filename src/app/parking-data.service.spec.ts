c1b8365f4c03b270b400bc466c71947d566bc439
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2018-01-01 17:08:19 +0100

    First tests for parking service. API tests need to be checked and extended.

--- src/app/parking-data.service.spec.ts
+++ src/app/parking-data.service.spec.ts
@@ -1,15 +1,40 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { ParkingDataService } from './parking-data.service';
+import { Http, Response, Headers, RequestOptions } from "@angular/http";
 
 describe('ParkingDataService', () => {
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ParkingDataService]
-    });
+        imports: [
+          HttpModule
+        ],
+        providers: [
+          ParkingDataService
+        ]
+      })
+      .compileComponents();
   });
 
-  /*it('should ...', inject([ParkingDataService], (service: ParkingDataService) => {
+  it('should be injectable', inject([ParkingDataService], (service: ParkingDataService) => {
     expect(service).toBeTruthy();
-  }));*/
+  }));
+
+  it('should set and get the server id (city name)', inject([ParkingDataService], (service: ParkingDataService) => {
+    let dummyServerId = 'aachen';
+    service.setServerId(dummyServerId);
+    expect(service.getServerId()).toEqual(dummyServerId);
+  }));
+
+  it('should fetch a list of available cities/servers', async(inject([ParkingDataService], (service: ParkingDataService) => {
+    service.getAvailableCities().subscribe(
+      cities => {
+        console.info(cities);
+        expect(typeof cities).toEqual('array');
+      }, err => expect(err).toBeNull()
+    );
+  })));
+
 });
