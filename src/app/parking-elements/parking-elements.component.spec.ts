0afb00e6e3c1566b6ee8e2eec659ca8fcf09eadf
Alexander Schmidt <alexander.schmidt1@rwth-aachen.de>
2018-01-07 22:14:38 +0100

    remove test description from parking elements component

--- src/app/parking-elements/parking-elements.component.spec.ts
+++ src/app/parking-elements/parking-elements.component.spec.ts
@@ -19,7 +19,7 @@ describe('ParkingElementsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  //it('should create', () => {
+  //  expect(component).toBeTruthy();
+  //});
 });
